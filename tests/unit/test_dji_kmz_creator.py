f936125f28157371f45f2051646004078c00f866
Marijn <marijnbrandwijk@hotmail.com>
2022-11-05 17:52:50 +0100

    added test file

---
+++ tests/unit/test_dji_kmz_creator.py
@@ -0,0 +1,6 @@
+from ...flightplanner import dji_kmz_creator
+
+if __name__ == '__main__':
+    point1 = dji_kmz_creator.dji_waypoint_mission(10, 4.233, 52.00)
+    print(type)
+    #not working yet
