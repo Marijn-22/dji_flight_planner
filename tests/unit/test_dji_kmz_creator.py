2c4810454345ca42ba6f2365a2bd5e6c08d88aad
Marijn <marijnbrandwijk@hotmail.com>
2022-11-07 09:55:22 +0100

    setting up testing script

--- tests/unit/test_dji_kmz_creator.py
+++ tests/unit/test_dji_kmz_creator.py
@@ -1,6 +1,18 @@
-from ...flightplanner import dji_kmz_creator
+
+# setting path for flightplanner
+import sys
+sys.path.append('./flightplanner')
+import dji_kmz_creator
+
+import unittest
+
+class TestSum(unittest.TestCase):
+
+    def test_1(self):
+        point1 = dji_kmz_creator.dji_waypoint_mission(10, 4.233, 52.00)
+        
 
 if __name__ == '__main__':
-    point1 = dji_kmz_creator.dji_waypoint_mission(10, 4.233, 52.00)
-    print(type)
-    #not working yet
+    unittest.main()
+
+
